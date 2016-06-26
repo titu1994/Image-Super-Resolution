c2032d9dc1a7d8e0b381bb664e8425292fdbcb32
Somshubra Majumdar <titu1994@gmail.com>
2016-06-26 16:37:13 +0530

    Updated tests.py

--- tests.py
+++ tests.py
@@ -14,8 +14,17 @@ if __name__ == "__main__":
     #plot(model, to_file="SRCNN.png", show_shapes=True, show_layer_names=True)
     #model = models.ExpantionSuperResolution().create_model()
     #plot(model, to_file="ESRCNN.png", show_layer_names=True, show_shapes=True)
-    model = models.DenoisingAutoEncoderSR().create_model()
-    plot(model, to_file="Denoise.png", show_layer_names=True, show_shapes=True)
+    #model = models.DenoisingAutoEncoderSR().create_model()
+    #plot(model, to_file="Denoise.png", show_layer_names=True, show_shapes=True)
+
+    """
+    Train Super Resolution
+    """
+    #trainX, trainY = img_utils.loadImages()
+
+    #sr = models.ImageSuperResolutionModel()
+    #sr.create_model()
+    #sr.fit(trainX, trainY, nb_epochs=100)
 
     """
     Train ExpantionSuperResolution
@@ -23,7 +32,7 @@ if __name__ == "__main__":
     #trainX, trainY = img_utils.loadImages()
 
     #esr = models.ExpantionSuperResolution()
-    #esr.create_model(load_weights=True)
+    #esr.create_model()
     #esr.fit(trainX, trainY, nb_epochs=100)
 
     """
@@ -31,10 +40,11 @@ if __name__ == "__main__":
     """
     # NOTE: Denoising auto encoder requires even integer height x width. Thus the training images are downscaled.
     #trainX, trainY = img_utils.loadDenoisingImages()
-    #denoise = models.DenoisingAutoEncoderSR()
-    #denoise.create_model(load_weights=False).summary()
 
-    #denoise.fit(trainX, trainY)
+    #dsr = models.DenoisingAutoEncoderSR()
+    #dsr.create_model(load_weights=False).summary()
+
+    #dsr.fit(trainX, trainY)
 
     """
     Compare output images of sr, esr and denoise models
