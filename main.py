c3bfd210abd6226fe2a7a47905a264dee939a227
Somshubra Majumdar <titu1994@gmail.com>
2016-06-26 15:26:53 +0530

    Added support for "dsr" in main.py

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description="Up-Scales an image using Image Super Resolution Model")
     parser.add_argument("imgpath", type=str, help="Path to input image")
-    parser.add_argument("--model", type=str, default="sr", help="Use either image super resolution (sr) or expanded super resolution (esr)")
+    parser.add_argument("--model", type=str, default="sr", help="Use either image super resolution (sr), "
+                        "expanded super resolution (esr) or denoising auto encoder sr (dsr)")
     parser.add_argument("--scale", default=2, help='Scaling factor. Default = 2x')
     parser.add_argument("--save_intermediate", dest='save', default='False', type='str',
                         help="Whether to save bilinear upscaled image")
@@ -16,10 +17,16 @@ if __name__ == "__main__":
 
     path = args.imgpath
     model_type = str(args.model).lower()
-    assert model_type in ["sr", "esr"], 'Model type must be either "sr" or "esr"'
+    assert model_type in ["sr", "esr", "dsr"], 'Model type must be either "sr", "esr" or "dsr"'
 
     scale_factor = args.scale
     save = strToBool(args.save)
 
-    model = models.ImageSuperResolutionModel() if model_type == "sr" else models.ExpantionSuperResolution()
+    if model_type == "sr":
+        model = models.ImageSuperResolutionModel()
+    elif model_type == "esr":
+        model = models.ExpantionSuperResolution()
+    else:
+        model = models.DenoisingAutoEncoderSR()
+
     model.upscale(path, scale_factor=scale_factor, save_intermediate=save)
